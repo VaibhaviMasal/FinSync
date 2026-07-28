95a762855c6c6f713277f6fc9e01ab28da5db554
VaibhaviMasal <142891383+VaibhaviMasal@users.noreply.github.com>
2026-07-28 12:51:27 +0530

    feat: add policy renewal request validators

---
+++ FinSync.Application/Features/PolicyRenewals/Validators/CreatePolicyRenewalRequestValidator.cs
@@ -0,0 +1,34 @@
+﻿using FinSync.Application.Features.PolicyRenewals.DTOs;
+using FluentValidation;
+
+namespace FinSync.Application.Features.PolicyRenewals.Validators
+{
+    public class CreatePolicyRenewalRequestValidator
+        : AbstractValidator<CreatePolicyRenewalRequestDto>
+    {
+        public CreatePolicyRenewalRequestValidator()
+        {
+            RuleFor(x => x.PolicyId)
+                .GreaterThan(0)
+                .WithMessage("Policy ID must be greater than zero.");
+
+            RuleFor(x => x.RenewalDate)
+                .NotEmpty()
+                .WithMessage("Renewal Date is required.");
+
+            RuleFor(x => x.NewExpiryDate)
+                .NotEmpty()
+                .WithMessage("New Expiry Date is required.")
+                .Must((dto, newExpiryDate) => newExpiryDate > dto.RenewalDate)
+                .WithMessage("New Expiry Date must be later than Renewal Date.");
+
+            RuleFor(x => x.RenewalPremium)
+                .GreaterThan(0)
+                .WithMessage("Renewal Premium must be greater than zero.");
+
+            RuleFor(x => x.Remarks)
+                .MaximumLength(500)
+                .WithMessage("Remarks cannot exceed 500 characters.");
+        }
+    }
+}
