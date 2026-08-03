3aa4bb7358de365a9a3918cbe964f1f002b41770
VaibhaviMasal <142891383+VaibhaviMasal@users.noreply.github.com>
2026-08-03 12:49:40 +0530

    refactor(report): add base policy query helper

--- FinSync.Domain/Entities/Policy.cs
+++ FinSync.Domain/Entities/Policy.cs
@@ -57,8 +57,8 @@ namespace FinSync.Domain.Entities
         public ICollection<PolicyRenewal> PolicyRenewals { get; set; }
     = new List<PolicyRenewal>();
 
-        public ICollection<InsuranceClaim> InsuaranceClaims { get; set; }
-            = new List<InsuranceClaim>();
+        public ICollection<InsuranceClaim> InsuranceClaims { get; set; }
+    = new List<InsuranceClaim>();
 
     }
 }
--- FinSync.Persistance/Repositories/ReportRepository.cs
+++ FinSync.Persistance/Repositories/ReportRepository.cs
@@ -1,5 +1,6 @@
 ﻿using FinSync.Application.Features.Reports.DTOs;
 using FinSync.Application.Features.Reports.Interfaces;
+using FinSync.Domain.Entities;
 using FinSync.Persistence.Context;
 using Microsoft.EntityFrameworkCore;
 
@@ -14,164 +15,50 @@ namespace FinSync.Persistence.Repositories
             _context = context;
         }
 
-        public async Task<IEnumerable<ActivePolicyReportDto>> GetActivePoliciesAsync(ReportFilterDto filter)
+        public Task<IEnumerable<ActivePolicyReportDto>> GetActivePoliciesAsync(ReportFilterDto filter)
         {
-            var query = _context.Policies
-    .Include(p => p.Customer)
-    .Include(p => p.Agent)
-    .Include(p => p.InsuranceCompany)
-    .Include(p => p.InsurancePlan)
-    .Where(p => p.PolicyStatus == "Active")
-    .AsQueryable();
-
-            if (filter.CompanyId.HasValue)
-            {
-                query = query.Where(p => p.CompanyId == filter.CompanyId.Value);
-            }
-
-            if (filter.AgentId.HasValue)
-            {
-                query = query.Where(p => p.AgentId == filter.AgentId.Value);
-            }
-
-            if (filter.FromDate.HasValue)
-            {
-                query = query.Where(p => p.StartDate >= filter.FromDate.Value);
-            }
-
-            if (filter.ToDate.HasValue)
-            {
-                query = query.Where(p => p.EndDate <= filter.ToDate.Value);
-            }
-
-            query = query
-                .Skip((filter.PageNumber - 1) * filter.PageSize)
-                .Take(filter.PageSize);
-
-            return await query.Select(p => new ActivePolicyReportDto
-            {
-                PolicyNumber = p.PolicyNumber,
-
-                CustomerName = p.Customer.FirstName + " " + p.Customer.LastName,
-
-                CompanyName = p.InsuranceCompany.CompanyName,
-
-                PlanName = p.InsurancePlan.PlanName,
-
-                StartDate = p.StartDate,
-
-                EndDate = p.EndDate,
-
-                PremiumAmount = p.PremiumAmount,
-
-                AgentName = p.Agent.FirstName + " " + p.Agent.LastName
-            })
-            .ToListAsync();
+            throw new NotImplementedException();
         }
 
-        public async Task<IEnumerable<ExpiringPolicyReportDto>> GetExpiringPoliciesAsync(ReportFilterDto filter)
+        public Task<IEnumerable<AgentPerformanceReportDto>> GetAgentPerformanceAsync(ReportFilterDto filter)
         {
-            var today = DateTime.Today;
-            var next30Days = today.AddDays(30);
+            throw new NotImplementedException();
+        }
 
-            var query = _context.Policies
+        public Task<IEnumerable<ClaimReportDto>> GetClaimReportAsync(ReportFilterDto filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<CompanyBusinessReportDto>> GetCompanyBusinessAsync(ReportFilterDto filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<DashboardReportDto> GetDashboardReportAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<ExpiringPolicyReportDto>> GetExpiringPoliciesAsync(ReportFilterDto filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<PremiumCollectionReportDto> GetPremiumCollectionAsync(ReportFilterDto filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        private IQueryable<Policy> GetPolicyReportQuery()
+        {
+            return _context.Policies
                 .Include(p => p.Customer)
                 .Include(p => p.Agent)
                 .Include(p => p.InsuranceCompany)
-                .Include(p => p.InsurancePlan)
-                .Where(p => p.EndDate >= today && p.EndDate <= next30Days)
-                .AsQueryable();
-
-            if (filter.CompanyId.HasValue)
-            {
-                query = query.Where(p => p.CompanyId == filter.CompanyId.Value);
-            }
-
-            if (filter.AgentId.HasValue)
-            {
-                query = query.Where(p => p.AgentId == filter.AgentId.Value);
-            }
-
-            query = query
-                .OrderBy(p => p.EndDate)
-                .Skip((filter.PageNumber - 1) * filter.PageSize)
-                .Take(filter.PageSize);
-
-            return await query.Select(p => new ExpiringPolicyReportDto
-            {
-                PolicyNumber = p.PolicyNumber,
-
-                CustomerName = p.Customer.FirstName + " " + p.Customer.LastName,
-
-                CompanyName = p.InsuranceCompany.CompanyName,
-
-                ExpiryDate = p.EndDate,
-
-                DaysRemaining = (p.EndDate.Date - today).Days,
-
-                PremiumAmount = p.PremiumAmount,
-
-                AgentName = p.Agent.FirstName + " " + p.Agent.LastName
-
-            }).ToListAsync();
+                .Include(p => p.InsurancePlan);
         }
 
-        public async Task<PremiumCollectionReportDto> GetPremiumCollectionAsync(ReportFilterDto filter)
-        {
-            var payments = _context.PremiumPayments.AsQueryable();
-
-            if (filter.FromDate.HasValue)
-            {
-                payments = payments.Where(p => p.CreatedDate >= filter.FromDate.Value);
-            }
-
-            if (filter.ToDate.HasValue)
-            {
-                payments = payments.Where(p => p.CreatedDate <= filter.ToDate.Value);
-            }
-
-            var totalPremium = await payments.SumAsync(x => x.Amount);
-
-            var paidPremium = await payments
-                .Where(x => x.PaymentStatus == Domain.Enums.PaymentStatus.Paid)
-                .SumAsync(x => x.Amount);
-
-            var pendingPremium = await payments
-                .Where(x => x.PaymentStatus == Domain.Enums.PaymentStatus.Pending)
-                .SumAsync(x => x.Amount);
-
-            return new PremiumCollectionReportDto
-            {
-                TotalPremiumCollected = totalPremium,
-                PaidPremium = paidPremium,
-                PendingPremium = pendingPremium,
-
-                TotalPayments = await payments.CountAsync(),
-
-                PaidPayments = await payments.CountAsync(x => x.PaymentStatus == Domain.Enums.PaymentStatus.Paid),
-
-                PendingPayments = await payments.CountAsync(x => x.PaymentStatus == Domain.Enums.PaymentStatus.Pending)
-            };
-        }
-
-        public async Task<IEnumerable<AgentPerformanceReportDto>> GetAgentPerformanceAsync(ReportFilterDto filter)
-        {
-            throw new NotImplementedException();
-        }
-
-        public async Task<IEnumerable<CompanyBusinessReportDto>> GetCompanyBusinessAsync(ReportFilterDto filter)
-        {
-            throw new NotImplementedException();
-        }
-
-        public async Task<IEnumerable<ClaimReportDto>> GetClaimReportAsync(ReportFilterDto filter)
-        {
-            throw new NotImplementedException();
-        }
-
-        public async Task<DashboardReportDto> GetDashboardReportAsync()
-        {
-            throw new NotImplementedException();
-        }
+        // Report methods will be added here...
     }
 }
